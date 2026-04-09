bde9ef1dceba79daa8a23c965911a1249901e788
Beatriz Omena <beatrizomena18@gmail.com>
2026-04-09 16:29:47 -0300

    ajustes no menu toggle

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -1,13 +1,23 @@
 const botao = document.querySelector('.button-menu-toggle');
 const menu = document.querySelector('.menu-toggle');
 const overlay = document.querySelector('.overlay');
+const saida = document.querySelector('.botaoSaida');
 
 botao.addEventListener('click', () => {
   menu.classList.toggle('ativo');
   overlay.classList.toggle('ativo');
+  saida.classList.toggle('ativo');
 });
 
 overlay.addEventListener('click', () => {
   menu.classList.remove('ativo');
   overlay.classList.remove('ativo');
+  saida.classList.remove('ativo');
 });
+
+saida.addEventListener('click', () => {
+  menu.classList.remove('ativo');
+  overlay.classList.remove('ativo');
+  saida.classList.remove('ativo');
+});
+
